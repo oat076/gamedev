e41314bcb8b1b5cf5bc09beae6fe8f0f12269600
oat076 <ollie.thorpe@papamoacollege.school.nz>
2024-10-20 21:52:33 +1300

    Made a reload

--- Assets/GunV2.cs
+++ Assets/GunV2.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class GunV2 : MonoBehaviour
 {
@@ -7,15 +8,37 @@ public class GunV2 : MonoBehaviour
     public float fireRate = 15f;
     public float impactForce = 30f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
 
+    public Animator animator;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
     // Update is called once per frame
     void Update()
     {
+
+        if (isReloading)
+            return;
+
+        if (currentAmmo < 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)  // making the button work
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -23,9 +46,27 @@ public class GunV2 : MonoBehaviour
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
         muzzleFlash.Play();
+
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) //shooting the bullet from the gun
         {
